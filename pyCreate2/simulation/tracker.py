67d42211927b3ebb29ab07df3bb744015db2c625
Chotiwat Chawannakul <chotiwat.c@hotmail.com>
2017-03-10 15:02:22 -0800

    add a noise model for the tracker in simulation

--- pyCreate2/simulation/tracker.py
+++ pyCreate2/simulation/tracker.py
@@ -1,4 +1,6 @@
 from ..vrep import vrep as vrep
+import math
+import numpy as np
 
 class Tracker:
   def __init__(self, client_id, tag_id, time, port = 5555, rate = 10):
@@ -15,11 +17,17 @@ class Tracker:
     t = self.time.time()
     if t > self.queried_time + 1.0 / self.rate:
       self.queried_time = t
+      if np.random.random() < 0.05: # 5% chance of not detecting the tag
+        return None
       rc, xyz = vrep.simxGetObjectPosition(
         self._clientID,
         self._create,
         -1,
         vrep.simx_opmode_oneshot)
+
+      x, y, z = xyz
+      x += np.random.normal(0, 0.01)
+      y += np.random.normal(0, 0.01)
 
       rc, rpy = vrep.simxGetObjectOrientation(
         self._clientID,
@@ -27,11 +35,14 @@ class Tracker:
         -1,
         vrep.simx_opmode_oneshot)
 
+      roll, pitch, yaw = rpy
+      roll = math.fmod(roll + np.random.normal(0, math.radians(0.01)), 2 * math.pi)
+
       return {
         'id': self.tag_id,
         'time': t,
-        'position': dict(zip('xyz', xyz)),
-        'orientation': dict(zip('rpy', rpy))
+        'position': dict(zip('xyz', (x, y, z))),
+        'orientation': dict(zip('rpy', (roll, pitch, yaw)))
       }
 
     return None
